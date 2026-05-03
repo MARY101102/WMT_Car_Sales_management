7a1cedec3161e6630c1867ab25c2e96d149c259f
Sandeera-2003 <sandeeragnawarathna@gmail.com>
2026-05-03 23:09:05 +0530

    Add state management with Zustand and AsyncStorage for authentication and cart functionality

---
+++ frontend/src/store/useStore.js
@@ -0,0 +1,32 @@
+import { create } from 'zustand';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const useStore = create((set, get) => ({
+  token: null,
+  user: null,
+  isAuthenticated: false,
+  cart: [],
+
+  checkAuth: async () => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        set({ token, isAuthenticated: true });
+        // Optionally fetch and set `user` here if your API supports it
+      } else {
+        set({ token: null, isAuthenticated: false, user: null });
+      }
+    } catch (e) {
+      set({ token: null, isAuthenticated: false, user: null });
+    }
+  },
+
+  addToCart: (item) => set(state => ({ cart: [...state.cart, item] })),
+  removeFromCart: (id) => set(state => ({ cart: state.cart.filter(i => i.id !== id) })),
+  updateCartQuantity: (id, quantity) => set(state => ({
+    cart: state.cart.map(i => (i.id === id ? { ...i, quantity } : i))
+  })),
+  clearCart: () => set({ cart: [] }),
+}));
+
+export default useStore;
